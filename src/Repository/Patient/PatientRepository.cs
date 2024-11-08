ee5277c6b16e9da797161ac7ce8d07cc0e1e279e
sxergiu <sxergiu@gmail.com>
2024-11-08 21:39:27 +0200

    Patient Repo - GetAll, GetById, Create

--- src/Repository/Patient/PatientRepository.cs
+++ src/Repository/Patient/PatientRepository.cs
@@ -1,6 +1,36 @@
+using MedicalAPI.Domain.Entities.User;
+using MedicalAPI.Repository.Database;
+using Microsoft.EntityFrameworkCore;
+
 namespace MedicalAPI.Repository.Patient;
 
-public class PatientRepository
+public class PatientRepository : IPatientRepository
 {
+    private readonly AppDbContext _context;
+
+    public PatientRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<PatientModel>> GetAllPatientsAsync()
+    {
+        return await _context.Patients
+            .Include( a => a.PatientAppointments)
+            .ToListAsync();
+    }
+
+    public async Task<PatientModel?> GetPatientByIdAsync(string patientId)
+    {
+        return await _context.Patients
+            .Where(patient => patient.Id == patientId)
+            .OrderBy(patient => patient.Fullname)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task CreatePatientAsync(PatientModel patientModel)
+    {
+        _context.Patients.Add(patientModel);
+        await _context.SaveChangesAsync();
+    }
     
 }
