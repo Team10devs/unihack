9a1925951ddcf6700a47d68b26bb3ac3f75b83fb
raul.candrea <raul.candrea@irian.ro>
2024-11-08 16:17:16 +0200

    frontend

---
+++ unihack-frontend/src/app/doctor-page/doctor-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-page.component.html',
+  styleUrl: './doctor-page.component.css'
+})
+export class DoctorPageComponent {
+
+}
---
+++ unihack-frontend/src/app/environment/environment.ts
@@ -0,0 +1,6 @@
+
+export const environments = {
+
+  apiUrl: "http://localhost:8080",
+
+}
