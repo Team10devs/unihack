3fbc8e1f210a54535be00a826ede1db5e48155ca
crystisor <cristian.faur@student.upt.ro>
2024-11-08 19:37:00 +0200

    Designed register page

--- unihack-frontend/src/app/app.routes.ts
+++ unihack-frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import {LoginPageComponent} from './login-page/login-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
 
 export const routes: Routes = [
+
+  {
+    path:"register",
+    component : RegisterPageComponent
+  },
   {
     path:"**",
     component : LoginPageComponent
-  }
+  },
 ];
