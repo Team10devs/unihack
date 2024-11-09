52cfcd7a8605f87a27285eef752b3d67d02d4a83
sx? <sxergiu@gmail.com>
2024-11-09 12:05:51 +0200

    "removed guest from userEnum + Appointment Controller"
    mapping should be moved in a file separate from the controllers, changed acces specifiers to mapping

---
+++ src/API/Controllers/AppointmentController.cs
@@ -0,0 +1,69 @@
+﻿using MedicalAPI.Controllers;
+using MedicalAPI.Domain.DTOs.Appointment;
+using MedicalAPI.Service.Firebase.Appointment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalAPI.API.Controllers;
+
+[Route("api/[controller]/Appointment")]
+[ApiController]
+
+public class AppointmentController : ControllerBase
+{
+    private readonly IAppointmentService _appointmentService;
+    
+    public AppointmentController(IAppointmentService appointmentService)
+    {
+        _appointmentService = appointmentService;
+    } 
+    
+    [HttpGet("ByPatientId")]
+    public async Task<ActionResult<IEnumerable<AppointmentResponse>>> GetAppointmentsByPatientId(string patientId)
+    {
+        try
+        {
+            var appointments = 
+                await _appointmentService.GetPatientAppointments(patientId);
+            
+            return Ok(appointments.Select(DoctorController.MapAppointmentResponse));
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+    
+    [HttpGet("ByDoctorId")]
+    public async Task<ActionResult<IEnumerable<AppointmentResponse>>> GetAppointmentsByDoctorId(string doctorId)
+    {
+        try
+        {
+            var appointments = 
+                await _appointmentService.GetDoctorAppointments(doctorId);
+            
+            return Ok(appointments.Select(DoctorController.MapAppointmentResponse));
+        }
+        catch (Exception ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpPost("CreateAppointment")]
+    public async Task<ActionResult<AppointmentResponse>> CreateAppointment(
+        [FromBody] AppointmentRequest appointmentRequest)
+    {
+        try
+        {
+            var createdAppointment =
+                await _appointmentService.CreateAppointment(appointmentRequest);
+            return Ok(DoctorController.MapAppointmentResponse(createdAppointment));
+        }
+        catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+    }
+    
+}
