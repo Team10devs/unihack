7652566f2c1e6a4b3f09e5ff722c47973afae2d6
Razvan Bitea <razvanbitea@gmail.com>
2024-11-10 11:56:43 +0200

    fix patient

--- src/Domain/Entities/Prescription/PrescriptionResponse.cs
+++ src/Domain/Entities/Prescription/PrescriptionResponse.cs
@@ -7,16 +7,18 @@ namespace MedicalAPI.Domain.Entities.Prescription;
 
 public class PrescriptionResponse
 {
-    public PrescriptionResponse(string prescriptionId, string patientId, string diagnostic, List<MedicineResponse> medicine)
+    public PrescriptionResponse(string prescriptionId, string patientId, string patientName, string diagnostic, List<MedicineResponse> medicine)
     {
         PrescriptionId = prescriptionId;
         PatientId = patientId;
+        this.patientName = patientName;
         Diagnostic = diagnostic;
         Medicine = medicine;
     }
 
     public string PrescriptionId { get; set; }
     public string PatientId { get; set; }
+    public string patientName { get; set; }
     public string Diagnostic { get; set; }
     public List<MedicineResponse> Medicine { get; set; }
 }
